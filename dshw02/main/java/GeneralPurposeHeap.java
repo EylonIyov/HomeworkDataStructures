8e070488a46ccd912a505fdddbdac38167355632
EylonIyov <eylon.iyov@runi.ac.il>
2024-04-21 12:38:50 +0300

    Changed the implementation of heap

--- dshw02/main/java/GeneralPurposeHeap.java
+++ dshw02/main/java/GeneralPurposeHeap.java
@@ -1,49 +1,51 @@
-import java.util.ArrayList;
 
-public class GeneralPurposeHeap <T extends Comparable> {
-    ArrayList<T> heap;
+public class GeneralPurposeHeap <T extends Comparable<T>> {
+    T[] heap;
     int size;
+    int capacity;
 
     public GeneralPurposeHeap() {
-    this.heap = new ArrayList<T>();
-    this.heap.set(0, null);
+    this.heap = (T[]) new Comparable[16];
+    this.heap[0] = null;
     this.size = 0;
+    this.capacity = 16;
     }
     public GeneralPurposeHeap(T SingleObject) {
-        this.heap = new ArrayList<T>();
-        this.heap.set(0, null);
-        this.heap.set(1, SingleObject);
+        this.heap = (T[]) new Comparable[16];
+        this.heap[0] = null;
+        this.heap[1] = SingleObject;
         this.size = 1;
+        this.capacity = 16;
     }
-    public void PercDown(GeneralPurposeHeap<T> MinHeap,T elementInserted,int index ){
-        if(2*index >  MinHeap.size){
-            MinHeap.heap.set(index, elementInserted);
+    public GeneralPurposeHeap(T[] ArrayToHeap) {
+        this.heap = (T[]) new Comparable[ArrayToHeap.length*2];
+        this.heap[0] = null;
+        System.arraycopy(ArrayToHeap, 0, this.heap, 1, ArrayToHeap.length);
+        for (int i = (this.size / 2) -1; i >= 0; i--) {
+            //PercDown(this, this.heap[i], );
         }
-        if(2*index == MinHeap.size){
-            if(MinHeap.heap.get(index).compareTo(elementInserted) == -1){
-                MinHeap.heap.set(index, MinHeap.heap.get(2*index));
-                MinHeap.heap.set(2*index, elementInserted);
-            }
-            else {
-                MinHeap.heap.set(index, elementInserted);
-            }
-        }
-        int j;
-        if(MinHeap.heap.get(index).compareTo(elementInserted) == 1){
-            if(MinHeap.heap.get(index*2).compareTo(MinHeap.heap.get(index*2+1)) == -1){
-                 j = index*2;
-            }
-            else{
-                j = index*2+1;
-            }
-            if(MinHeap.heap.get(j).compareTo(elementInserted) == -1){
-                MinHeap.heap.set(index, MinHeap.heap.get(j));
-                PercDown(MinHeap,elementInserted,j);
-            }else{
-                MinHeap.heap.set(index, elementInserted);
-            }
-        }
+
     }
+    public void percDown(int index) {
+    int LeftChild = 2 * index;
+    int RightChild = LeftChild + 1;
+    int current = index;
+    if (LeftChild < size && heap[LeftChild].compareTo(heap[RightChild]) < 0 ) {}
+    }
+    public void ExpandHeap(GeneralPurposeHeap<T> MinHeap){
+        int newCapacity = MinHeap.capacity*2;
+        T[] newHeap = (T[]) new Comparable[newCapacity];
+        for(int i = 0; i < MinHeap.size; i++) newHeap[i] = MinHeap.heap[i];
+        MinHeap.heap = newHeap;
+        MinHeap.capacity = newCapacity;
+        }
+    public void swap(int i, int j){
+        T temp = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = temp;
+    }
+    }
+
 
     }
 
