0447cadad9f72a3e86d1e3df45e88ccab5ee281b
EylonIyov <eylon.iyov@runi.ac.il>
2024-04-25 11:58:01 +0300

    Changed the array to start from index 1 and not 0

--- dshw02/main/java/GeneralPurposeHeap.java
+++ dshw02/main/java/GeneralPurposeHeap.java
@@ -18,51 +18,61 @@ public class GeneralPurposeHeap <T extends Comparable<T>> {
     }
 
     public GeneralPurposeHeap(T[] ArrayToHeap) {
-        this.heap = (T[]) new Comparable[ArrayToHeap.length];
+        this.heap = (T[]) new Comparable[(ArrayToHeap.length + 1)];
         this.size = ArrayToHeap.length;
         this.capacity = ArrayToHeap.length;
-        System.arraycopy(ArrayToHeap, 0, this.heap, 0, ArrayToHeap.length);
+        System.arraycopy(ArrayToHeap, 0, this.heap, 1, ArrayToHeap.length);
         sort(this.heap);
-        }
+    }
 
     private void PercDown(int index, T value, int n, T[] heap) {
-       int left = index * 2 + 1;
-       int right = left + 1;
-       int j =0;
-       if (left <= n) heap[index] = value;
-       if(left == n){
-           if (heap[left].compareTo(value) < 0 && heap[left] != null) {
-               heap[index] = heap[left];
-               heap[left] = value;
-           }else{
-               heap[index] = value;
-           }
+        int left = index * 2;
+        int right = left + 1;
+        int j = 0;
+        if (left > n) heap[index] = value;
+        if (left == n) {
+            if  (heap[left] != null && heap[left].compareTo(value) < 0 ) {
+                heap[index] = heap[left];
+                heap[left] = value;
+            } else {
+                heap[index] = value;
+            }
         }
-       if(left<n){
-           if(heap[left].compareTo(heap[right]) < 0 && heap[left] != null && heap[right] != null) {
-               j = left;
-           }else if (heap[right] != null){
-               j = right;
-           }
-           if (j>0 && heap[j] != null && heap[j].compareTo(value) < 0) {
-               heap[index] = heap[j];
-               PercDown(j, value, n, heap);
-           }else{
-               heap[index] = value;
-           }
-       }
-    }
-private void percUp(T[] heap, int index) {
-    while (index > 0) {
-        int parentIndex = (index - 1) / 2;
-        if (heap[parentIndex] != null  && heap[index] != null  && heap[index].compareTo(heap[parentIndex]) < 0) {
-            swap(index, parentIndex);
-            index = parentIndex;
-        } else {
-            break;
+        if (left < n) {
+            if (heap[left] != null && heap[right] != null && heap[left].compareTo(heap[right]) < 0) {
+                j = left;
+            } else if (heap[right] != null) {
+                j = right;
+            }
+            if (j > 1 && heap[j] != null && heap[j].compareTo(value) < 0) {
+                heap[index] = heap[j];
+                PercDown(j, value, n, heap);
+            } else {
+                heap[index] = value;
+            }
         }
     }
-}
+
+    private void percUp(T[] heap, int index) {
+        while (index > 0) {
+            int parentIndex = (index) / 2;
+            if (heap[parentIndex] != null && heap[index] != null && heap[index].compareTo(heap[parentIndex]) < 0) {
+                swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    public T deleteMin(){
+        T temp = this.getMin();
+        this.heap[1] = this.heap[size];
+        this.heap[size] = null;
+        PercDown(1, heap[1], size, heap );
+        this.size--;
+        return temp;
+    }
+
 
     private void ExpandHeap() {
         int newCapacity = this.capacity * 2;
@@ -77,43 +87,67 @@ private void percUp(T[] heap, int index) {
         this.heap[i] = this.heap[j];
         this.heap[j] = temp;
     }
+
     public void insert(T Object) {
         if (size == capacity) {
-            System.out.println("Heap is full, previous capacity: " + this.capacity + " New capacity: " + this.capacity*2);
+            System.out.println("Heap is full, previous capacity: " + this.capacity + " New capacity: " + this.capacity * 2);
             this.ExpandHeap();
         }
-            heap[size] = Object;
-            percUp(this.heap, size);
-            this.size++;
-        }
+        heap[size] = Object;
+        percUp(this.heap, size);
+        this.size++;
+    }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
-    public T getMin(){
-        if(size ==0) throw new IllegalStateException("Heap is empty");
-        else return heap[0];
-    }
-    private int GetMinChildIndex(int index){
 
-        if (((index*2)+1) > size && heap[index] != null ) return index*2;
-        else{
-            if(heap[index*2].compareTo(heap[index*2+1]) > 0) return index*2;
-            else{
-                return index*2 +1;
+    public T getMin() {
+        if (size == 0 || heap[1] == null) throw new IllegalStateException("Heap is empty");
+        else return heap[1];
+    }
+
+    private int GetMinChildIndex(int index) {
+
+        if (((index * 2) + 1) > size && heap[index] != null) return index * 2;
+        else {
+            if (heap[index * 2].compareTo(heap[index * 2 + 1]) > 0) return index * 2;
+            else {
+                return index * 2 + 1;
             }
 
         }
 
     }
-    private void sort (T[] Object){
+
+    private void sort(T[] Object) {
         int n = Object.length;
-        for (int i = (n/2) -1 ; i >= 0; i--) {
-            PercDown(i, Object[i], n-1, Object);
+        for (int i = (n / 2) ; i >= 0; i--) {
+            PercDown(i, heap[i], this.size, this.heap );
         }
     }
 
+
+
+    private boolean isLeaf(int index) {
+        if (index * 2 > size) return true;
+        else return false;
+    }
+
+    private boolean hasRightChild(int index) {
+        if (index * 2 + 1 < size) return true;
+        else return false;
+    }
+
+    private boolean hasLeftChild(int index) {
+        if (index * 2 < size) return true;
+        else return false;
+    }
+
+
 }
+
+
 
 
 
--- dshw02/main/java/TesterClass.java
+++ dshw02/main/java/TesterClass.java
@@ -15,7 +15,7 @@ public class TesterClass {
         String TestCase = " ";
         while (!TestCase.equals("exit")){
             System.out.println("Enter a case to test \n 1. Build an empty heap \n 2. Build a heap with a capacity argument \n 3. Build a heap from an array \n 4. Insert function \n" +
-                    " 5. Get minimum value \n 'exit' to exit" );
+                    " 5. Get minimum value \n 6. deleteMin test \n 'exit' to exit" );
             TestCase = new Scanner(System.in).nextLine();
             switch (TestCase) {
                 case "1":
@@ -25,17 +25,17 @@ public class TesterClass {
                 case "2":
                     System.out.println("Enter the capacity of the array");
                     Integer capacity = new Scanner(System.in).nextInt();
-                    GeneralPurposeHeap<Integer> TestHeap1 = new GeneralPurposeHeap<Integer>();
+                    GeneralPurposeHeap<Integer> TestHeap1 = new GeneralPurposeHeap<Integer>(capacity);
                     System.out.println(Arrays.toString(TestHeap1.heap));
                     break;
                 case "3":
-                    Integer[] exampleArray = GenerateArray(16, 1, 16);
+                    Integer[] exampleArray = GenerateArray(10, 1, 100);
                     System.out.println("Array before sorting into heap: \n"+Arrays.toString(exampleArray));
                     GeneralPurposeHeap<Integer> TestHeap2 = new GeneralPurposeHeap<Integer>(exampleArray);
                     System.out.println("Array after sorting into heap: \n"+Arrays.toString(TestHeap2.heap));
                     break;
                 case "4":
-                    Integer[] ArrayInteger = {5,7,6,9,1,4,1,6};
+                    Integer[] ArrayInteger = {5,7,6,9,1,4,1,6,null};
                     GeneralPurposeHeap<Integer> TestHeap3 = new GeneralPurposeHeap<Integer>(ArrayInteger);
                     System.out.println("Write number to insert");
                     Integer number = new Scanner(System.in).nextInt();
@@ -48,6 +48,13 @@ public class TesterClass {
                     GeneralPurposeHeap<Integer> TestHeap4 = new GeneralPurposeHeap<Integer>(ArrayInteger1);
                     System.out.println(Arrays.toString(TestHeap4.heap));
                     System.out.println(TestHeap4.getMin());
+                    break;
+                case "6":
+                    Integer[] ArrayInteger2 = GenerateArray(8,1,100);
+                    GeneralPurposeHeap<Integer> TestHeap5 = new GeneralPurposeHeap<Integer>(ArrayInteger2);
+                    System.out.println("The array before the deletion: \n"+ Arrays.toString(TestHeap5.heap) + "\n");
+                    System.out.println("The minimal value: " + TestHeap5.deleteMin() + '\n');
+                    System.out.println("The array after the deletion: \n" + Arrays.toString(TestHeap5.heap) + "\n");
                     break;
                 case "exit":
                     System.out.println("Exiting program, Thanks for using the TesterClass made by Eylon Iyov");
